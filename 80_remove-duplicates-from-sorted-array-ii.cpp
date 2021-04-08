180bb0722ae9f068f2d3035c0ece45c3d5782333
BillXSY <714017703@qq.com>
2021-04-08 15:50:16 +0800

    21.04.08 双指针

--- 80_remove-duplicates-from-sorted-array-ii.cpp
+++ 80_remove-duplicates-from-sorted-array-ii.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0;
+        if (!nums.size()) return 0;
+        int i = 0, dup = 1;
         for (int j = 1; j < nums.size(); ++j) {
             if (nums[i] != nums[j]){
-                i++;
-                nums[i] = nums[j];
+                if (dup >= 2){
+                    nums[i+1] = nums[i];
+                    nums[i+2] = nums[j];
+                    i+=2;
+                }
+                else nums[++i] = nums[j];
+                dup = 1;
+            }
+            else{
+                dup++;
+                nums[i+1] = nums[i];
             }
         }
-        return i + 1;
+        return i + (dup > 1 ? 2 : 1);
     }
 };
-
