8eae1ae93d3592bf85cc85e9a7a62c9d07c4e656
BillXSY <714017703@qq.com>
2021-04-11 18:30:02 +0800

    dynamic planning

--- 264_ugly-number-ii.cpp
+++ 264_ugly-number-ii.cpp
@@ -8,18 +8,12 @@ public:
         int p2 = 1, p3 = 1, p5 = 1;
         dp[1] = 1;
         for (int i = 2; i <= n; ++i) {
-            int num2 = dp[p2] * 2;
-            int num3 = dp[p3] * 3;
-            int num5 = dp[p5] * 5;
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
             dp[i] = min(min(num2,num3),num5);
-            // cout << "p2: " << p2 << " p3: " << p3 <<" p5: " << p5 << ' '<< dp[i] << '\n';
             if (dp[i] == num2) p2++;
             if (dp[i] == num3) p3++;
-            if (dp[i] == num5) p5++;
-
+            if (dp[i] == num5) p5++;//这里一定不能用else if
         }
         return dp[n];
-
-
     }
 };
