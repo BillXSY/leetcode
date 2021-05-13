9f963cd3f089621e7a727c86fce7f404f1b3aa2e
BillXSY <714017703@qq.com>
2021-05-14 00:50:03 +0800

    Dynamic Programming

--- 1269_number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269_number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -5,7 +5,7 @@
 class Solution {
 public:
     int numWays(int steps, int arrLen) {
-        arrLen = min (arrLen,251);
+        arrLen = min(arrLen, 251);
         vector<int> dp(arrLen + 2);
         dp[0] = 0;
         dp[1] = 1;
@@ -13,12 +13,10 @@ public:
         for (int i = 1; i < steps; ++i) {
             vector<int> temp(arrLen + 2);
             for (int j = 1; j < arrLen + 1; ++j) {
-                temp[j] = ((dp[j-1] + dp[j]) % int(1e9 + 7)  + dp[j+1])   % int(1e9 + 7);
+                temp[j] = ((dp[j - 1] + dp[j]) % int(1e9 + 7) + dp[j + 1]) % int(1e9 + 7);
             }
             dp = temp;
         }
-
-
         return dp[1];
     }
 };
