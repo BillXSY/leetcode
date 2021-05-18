db3257c7d331da1e0731fab28dbd4428d78335ad
BillXSY <714017703@qq.com>
2021-05-18 10:45:52 +0800

    0518

---
+++ cmake-build-debug/CMakeFiles/leetcodes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcodes.dir/main.cpp.obj"
+  "leetcodes.exe"
+  "leetcodes.exe.manifest"
+  "leetcodes.pdb"
+  "libleetcodes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcodes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
