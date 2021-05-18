0b59c1b1dab4245cf18e16fac1adfbed5a1476d4
BillXSY <714017703@qq.com>
2021-05-18 17:38:30 +0800

    TODO

---
+++ 1482_minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 71401 on 2021/5/18.
+//
+
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int flower_num = bloomDay.size();
+        if (m * k > flower_num) return -1;
+        vector<bool> yard(flower_num);
+        int day = 0;
+
+    }
+};
