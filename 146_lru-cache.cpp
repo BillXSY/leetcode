9fd6fea5125adc67c3b48fb6c005c0a298d0752b
BillXSY <714017703@qq.com>
2021-07-21 16:10:50 +0800

    0721

---
+++ 146_lru-cache.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 71401 on 2021/7/20.
+//
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+
+    }
+
+    int get(int key) {
+
+    }
+
+    void put(int key, int value) {
+
+    }
+};
