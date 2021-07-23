c42f3fe5fbd0d88b0cbcdf9dd0df3ffe8fdb7408
BillXSY <714017703@qq.com>
2021-07-23 13:24:28 +0800

    TODO

---
+++ 199_binary-tree-right-side-view.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 71401 on 2021/7/23.
+//
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode *root) {
+
+    }
+};
---
+++ 25_reverse-nodes-in-k-group.cpp
@@ -0,0 +1,34 @@
+//
+// Created by 71401 on 2021/7/23.
+//
+
+
+//  Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        ListNode *prev = nullptr;
+        ListNode *curr = head;
+        ListNode *next;
+        int count = 0;
+        while (curr && count < k) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        return prev;
+    }
+};
